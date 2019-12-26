7b728343047d73ec35d2659839d88ee93110d0cd
Ivan Bindusov <ivanbindusov@yandex.ru>
2019-12-26 22:54:53 +0400

    Hotfix meta tag

--- add.php
+++ add.php
@@ -26,7 +26,7 @@ if(isset($_POST["submit"])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Портфолио">
+    <meta name="description" content="Мини блог для портфолио">
     <link rel="stylesheet" href="/style/bootstrap-grid.min.css">
     <link rel="stylesheet" href="/style/bootstrap.min.css">
     <link rel="stylesheet" href="/style/main.css">
